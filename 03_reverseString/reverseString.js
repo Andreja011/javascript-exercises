fe744911073ea74e06c641004d2f09197a533b2a
Andreja011 <andrejasimonovic011@gmail.com>
2022-07-27 17:55:03 +0200

    Results of the 3rd excersise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function (string) {
+  const arr = [...string];
+  const reversedArr = arr.reverse();
+  const reversedArrStr = reversedArr.join("");
+  return reversedArrStr;
 };
 
 // Do not edit below this line
