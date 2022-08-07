6e5e8f3b05bafc8b1a7b385a6f933395670df040
Andreja011 <andrejasimonovic011@gmail.com>
2022-08-07 22:03:14 +0200

    Final solution for given problem

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
-
+const sumAll = function (start, end) {
+  if (typeof start !== "number" || typeof end !== "number") {
+    return "ERROR";
+  }
+  let max = Math.max(start, end);
+  let min = Math.min(start, end);
+  let sumBetween = 0;
+  for (let i = min; i <= max; i++) {
+    sumBetween += i;
+  }
+  if (sumBetween > 0) {
+    return sumBetween;
+  } else if (sumBetween < 0) {
+    return "ERROR";
+  }
 };
 
 // Do not edit below this line
