48e1ec2ee0a02d0d434c778a4634e9b1561bdbba
Andreja011 <andrejasimonovic011@gmail.com>
2022-08-08 21:07:11 +0200

    Solution for 6th excersise

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,14 @@
-const leapYears = function() {
-
+const leapYears = function (year) {
+  const divByFour = year % 4 === 0;
+  const divByHundred = year % 100 === 0;
+  const divByFourHundred = year % 400 === 0;
+  if (divByFour && divByHundred && divByFourHundred) {
+    return true;
+  } else if (divByFour && divByHundred) {
+    return false;
+  } else if (divByFour && !divByHundred) {
+    return true;
+  } else return false;
 };
 
 // Do not edit below this line
