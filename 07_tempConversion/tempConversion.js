08962ec1ae1dba135d1c96369d9a1d6f20676a6f
Andreja011 <andrejasimonovic011@gmail.com>
2022-08-09 21:28:24 +0200

    Solution for the last excersise in this part of The Odin Project

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,15 @@
-const ftoc = function() {
-
+const ftoc = function (fTemp) {
+  const celsiusTemp = Math.round(((fTemp - 32) / 1.8) * 10) / 10;
+  return celsiusTemp;
 };
 
-const ctof = function() {
-
+const ctof = function (cTemp) {
+  const fahrenheitTemp = Math.round((cTemp * (9 / 5) + 32) * 10) / 10;
+  return fahrenheitTemp;
 };
 
 // Do not edit below this line
 module.exports = {
   ftoc,
-  ctof
+  ctof,
 };
