e01d28fdb46abf1adf41d596765f69224838d703
Andreja <80786836+Andreja011@users.noreply.github.com>
2022-11-29 22:43:55 +0100

    Solution for calculator exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,27 +1,31 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (arr) {
+  return arr.reduce((total, num) => total + num, 0);
 };
 
-const multiply = function() {
-
+const multiply = function (arr) {
+  return arr.reduce((total, num) => total * num, 1);
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+  return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+  return !(num > 1) ? 1 : factorial(num - 1) * num;
+  // if (num < 0) return -1;
+  // else if (num == 0) return 1;
+  // else {
+  //   return num * factorial(num - 1);
+  // }
 };
-
 // Do not edit below this line
 module.exports = {
   add,
@@ -29,5 +33,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
