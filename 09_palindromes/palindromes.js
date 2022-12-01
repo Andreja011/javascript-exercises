f48d2bd9c65038b89f451571a5f7746de5710347
Andreja <80786836+Andreja011@users.noreply.github.com>
2022-12-01 23:46:13 +0100

    Solution for palindromes exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  let removeChar = str.replace(/[^A-Z0-9]/gi, "").toLowerCase();
+  let checkPalindrome = removeChar.split("").reverse().join("");
+  if (removeChar === checkPalindrome) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 // Do not edit below this line
