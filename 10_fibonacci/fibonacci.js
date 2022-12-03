79fe60dc7b6a04d0fd0e80b50fffef634172b887
Andreja <80786836+Andreja011@users.noreply.github.com>
2022-12-03 02:01:27 +0100

    Solution for fibonacci number

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function (num) {
+  if (num === 0 || num === 1) {
+    return Number(num);
+  } else if (num < 0) {
+    return "OOPS";
+  }
 
+  const result = [0, 1];
+
+  for (let i = 2; i <= num; i++) {
+    result[i] = result[i - 2] + result[i - 1];
+  }
+
+  return result[result.length - 1];
 };
 
 // Do not edit below this line
