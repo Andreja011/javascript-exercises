a083c6f900ee87641cf81ed176f8055adaf29bc6
Andreja <80786836+Andreja011@users.noreply.github.com>
2022-12-04 23:45:12 +0100

    Solution for the first test

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,11 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (arr) {
+  const oldest = arr.sort(function (a, b) {
+    const lastGuy = a.yearOfDeath - a.yearOfBirth;
+    const nextGuy = b.yearOfDeath - b.yearOfBirth;
+    return lastGuy > nextGuy ? -1 : 1;
+  });
+  console.log(oldest);
+  return oldest[0];
 };
 
 // Do not edit below this line
