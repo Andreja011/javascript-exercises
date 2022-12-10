5745259a5d0a7ed58a78e05f25142877f9f8ddf5
Andreja <80786836+Andreja011@users.noreply.github.com>
2022-12-10 12:20:32 +0100

    Final solution for all 3 tests

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,10 +1,13 @@
 const findTheOldest = function (arr) {
   const oldest = arr.sort(function (a, b) {
+    const currentYear = new Date().getFullYear();
+    arr.forEach((people) => {
+      return (people.yearOfDeath ??= currentYear);
+    });
     const lastGuy = a.yearOfDeath - a.yearOfBirth;
     const nextGuy = b.yearOfDeath - b.yearOfBirth;
     return lastGuy > nextGuy ? -1 : 1;
   });
-  console.log(oldest);
   return oldest[0];
 };
 
